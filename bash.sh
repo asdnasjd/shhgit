789595dd703a4f40a57f12c62e91766a5896312e
asdnasjd <82928216+asdnasjd@users.noreply.github.com>
2021-04-27 13:24:17 +0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i -e 's/socks4/socks5/g' /etc/proxychains.conf
-sed -i -e 's/127.0.0.0/45.63.115.127/g' /etc/proxychains.conf
+sed -i -e 's/127.0.0.1/45.63.115.127/g' /etc/proxychains.conf
 sed -i -e 's/9050/1080/g' /etc/proxychains.conf
